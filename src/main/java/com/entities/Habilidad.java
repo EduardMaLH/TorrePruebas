380d66f12d58fd49d569e30039eb1550300aca24
Eduard1151456 <eduardmauriciolh@ufps.edu.co>
2023-12-27 16:58:26 -0500

    entidades completas con relaciones

--- src/main/java/com/entities/Habilidad.java
+++ src/main/java/com/entities/Habilidad.java
@@ -1,5 +1,24 @@
 package com.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
 public class Habilidad {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String descripcion;
+    private String nuuid;
+
+    @ManyToOne
+    @JoinColumn(name = "rango_id")
+    private Rango rango;
+
+    @ManyToMany
+    @MapsId("jugador_Id")
+    private Jugador jugador;
 }
--- src/main/java/com/entities/Rango.java
+++ src/main/java/com/entities/Rango.java
@@ -1,5 +1,17 @@
 package com.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
 public class Rango {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String descripcion;
+    private String nuuid;
+
+  
 }
