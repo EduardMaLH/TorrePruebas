118a7e4d0d0a755cd9db2856962877137cb69204
Eduard1151456 <eduardmauriciolh@ufps.edu.co>
2023-12-27 16:51:05 -0500

    entidad Jugador

---
+++ src/main/java/com/entities/Jugador.java
@@ -0,0 +1,34 @@
+package com.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+public class Jugador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private Date fechaNacimiento;
+    private String descripcion;
+    private String genero;
+    private String nuuid;
+
+    @ManyToOne
+    @JoinColumn(name = "clase_id")
+    private Clase clase;
+
+    @ManyToOne
+    @JoinColumn(name = "rango_id")
+    private Rango rango;
+    
+    @ManyToMany
+    @MapsId("habilidad_Id")
+    private Habilidad habilidad;
+
+    
+}
