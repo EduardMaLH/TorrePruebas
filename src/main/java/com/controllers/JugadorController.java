73f6df1ec78a84191bf0c2c2042370a3fab2d3ec
Eduard1151456 <eduardmauriciolh@ufps.edu.co>
2023-12-27 19:34:04 -0500

    POST login

--- src/main/java/com/controllers/JugadorController.java
+++ src/main/java/com/controllers/JugadorController.java
@@ -1,6 +1,8 @@
 package com.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.entities.Jugador;
@@ -9,6 +11,7 @@ import com.repositories.JugadorRepository;
 import com.repositories.RangoRepository;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/jugadores")
@@ -17,9 +20,25 @@ public class JugadorController {
     @Autowired
     private JugadorRepository jugadorRepository;
 
-    @GetMapping
+    @GetMapping("/status")
     public String getStatus() {
         return "ok";
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Map<String, String> requestBody) {
+        
+        String nombre = requestBody.get("nombre");
+
+        
+        String nuuid = jugadorRepository.findNuuidByNombre(nombre);
+
+       
+        if (nuuid != null) {
+            return ResponseEntity.ok(nuuid);
+        } else {
+           
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Jugador no encontrado");
+        }
     }
 
   
--- src/main/java/com/repositories/JugadorRepository.java
+++ src/main/java/com/repositories/JugadorRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface JugadorRepository extends JpaRepository<Jugador, Integer> {
     List<Jugador> findByClase(Clase clase);
     List<Jugador> findByRango(Rango rango);
+	String findNuuidByNombre(String nombre);
     
 }
 
